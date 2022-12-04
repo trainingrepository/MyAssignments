1413e9fed200e626203112e21dd7e45df9d3ea4f
trainingrepository <119770383+trainingrepository@users.noreply.github.com>
2022-12-04 08:12:19 +0530

    Add files via upload

---
+++ day1/Calculations.java
@@ -0,0 +1,16 @@
+package week1.day1;
+
+public class Calculations {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Maths calc=new Maths();
+
+		System.out.println("sum of 2 numbers" + calc.add(2, 3));
+		System.out.println("sub of 2 numbers" + calc.sub(5, 3));
+		System.out.println("div of 2 numbers" + calc.div(5, 3));
+	}
+
+}
+
